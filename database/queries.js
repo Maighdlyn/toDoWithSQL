588ef3987a5384c46bf79101a907de95c1044061
Trevor Little <trevor@learnersguild.org>
2017-07-18 11:30:05 -0400

    code review comments

--- database/queries.js
+++ database/queries.js
@@ -1,3 +1,15 @@
+// instead of just putting the raw SQL queries here I think it would be even better
+// to export from this file functions that wrap and execute thse queries like:
+//
+// function add (taskName) {
+//   return db.none(
+//     'INSERT INTO to_do (task) VALUES ($1)',
+//     [taskName]
+//   ).then(() => taskName)
+// }
+//
+// ...etc
+
 const queries = {
   add: 'INSERT INTO to_do (task) VALUES ($1)',
   list: 'SELECT * FROM to_do ORDER BY id',
--- database/schema.sql
+++ database/schema.sql
@@ -8,12 +8,15 @@ DROP TABLE IF EXISTS to_do;
 CREATE TABLE to_do (
   id SERIAL PRIMARY KEY,
   task TEXT,
+  -- looks like this completed flag is not used
   completed BOOLEAN DEFAULT false
 );
 
 DROP DATABASE IF EXISTS to_do_list_test;
 CREATE DATABASE to_do_list_test;
 
+-- instead of repeating your schema for the test maybe pull in a common schema
+-- file for both environments and specify the db name in the command
 \c to_do_list_test
 
 DROP TABLE IF EXISTS to_do;
